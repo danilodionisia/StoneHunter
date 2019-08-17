d1599f83fc1264ac5dba2a11a7e923675751024e
Danilo Dionisia <danilo.dionisia@yahoo.com.br>
2019-08-16 22:29:42 -0300

    primeira versao

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject panel;
+    public AudioSource explosao;
+    public Text placar;
+    int pontos;
+    string textPontos;
+
+    void Start()
+    {
+        panel.SetActive(false);
+        pontos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pontos == 1)
+        {
+            textPontos = " Ponto";
+        }
+        else
+        {
+            textPontos = " Pontos";
+        }
+
+        placar.text = pontos + textPontos;
+
+        if (transform.position.y < -12f || transform.position.x < -20f || transform.position.y > 20f)
+        {
+            Destroy(this.gameObject);
+            panel.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "flip" && collision.gameObject.tag != "woodside")
+        {
+            if (collision.gameObject.tag == "stone")
+            {
+                pontos += 1;
+                explosao.Play();
+            }
+
+            collision.rigidbody.constraints = new RigidbodyConstraints2D();
+            collision.collider.isTrigger = true;
+        }
+    }
+}
---
+++ Assets/Script/Flip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Flip : MonoBehaviour
+{
+    public Joystick joystickLeft, joystickRight;
+    
+    float velocidade;
+    void Start()
+    {
+        velocidade = 0.15f;        
+    }
+
+    
+    void Update()
+    {
+        if (!Stone.isPause)
+        {
+            if (joystickLeft.Horizontal != 0)
+            {
+                transform.Translate(Vector3.left * velocidade);
+            }
+            else if (joystickRight.Horizontal != 0)
+            {
+                transform.Translate(Vector3.right * velocidade);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * 0);
+                transform.Translate(Vector3.left * 0);
+            }
+        }
+    }
+
+   
+}
